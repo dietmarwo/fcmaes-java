c18c9f8f9a34575eddecdcf33e20d059ebdb6504
dietmarwo <drdietmarwolz@gmail.com>
2021-04-15 16:52:17 +0200

    v1.2.19

--- temporal/src/main/java/fcmaes/temporal/core/Utils.java
+++ temporal/src/main/java/fcmaes/temporal/core/Utils.java
@@ -1,3 +1,8 @@
+/* Copyright (c) Dietmar Wolz.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory.
+ */
+
 package fcmaes.temporal.core;
 
 import fcmaes.core.Fitness;
